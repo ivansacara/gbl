894e7fc1ed6b1b5b3c1a6e485ebb536887876ba7
ivan <63603644+ivansacara@users.noreply.github.com>
2024-09-16 15:57:10 +0300

    captcha.php

---
+++ php/captcha.php
@@ -0,0 +1,17 @@
+<?php
+
+$secret   = "6Leocg8aAAAAAI1rISpeD4qNYU64lXdH1fFXNTtg";
+$response = null;
+
+require_once './classes/recaptcha/src/autoload.php';
+
+$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+$resp      = $recaptcha->setExpectedHostname('affrepublic.com')
+  ->verify($_POST["g-recaptcha-response"], $_SERVER["REMOTE_ADDR"]);
+if (!$resp->isSuccess()) {
+  $errors = $resp->getErrorCodes();
+  echo '<div class="err">' . $errors[0] . '</div>';
+  exit(0);
+} else {
+  $response = 'OK';
+}
